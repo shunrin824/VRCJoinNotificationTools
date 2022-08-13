96288c378a5bb6efcb186d68984377da1784bdf7
shunrin824 <shunrin@mail.shunrin.com>
2022-08-13 13:10:06 +0900

    ほぼ完成

--- Program.cs
+++ Program.cs
@@ -54,7 +54,7 @@ while (true)
                     if (LogDataLine.Contains("[Behaviour] OnPlayerJoined"))
                     {
                         NumberOfPlayer++;
-                        Console.WriteLine(LogDataLine.Substring(0,4) + "-" + LogDataLine.Substring(5,2) + "-" + LogDataLine.Substring(8,2) + "_" + LogDataLine.Substring(11,8) + " [Join] " + string.Format("[{0,2}] ", NumberOfPlayer) + LogDataLine.Substring(61));
+                        Console.WriteLine(LogDataLine.Substring(0, 4) + "-" + LogDataLine.Substring(5, 2) + "-" + LogDataLine.Substring(8, 2) + "_" + LogDataLine.Substring(11, 8) + " [Join] " + string.Format("[{0,2}] ", NumberOfPlayer) + LogDataLine.Substring(61));
                         JoinData = string.Concat(JoinData, "[" + LogDataLine.Substring(61) + "]");
                         JoinNotification = 1;
                         OscParameter.SendAvatarParameter("nsfw", false);
@@ -87,7 +87,7 @@ while (true)
             int.TryParse(DateTime.Now.ToString("mm"), out NowTime);
             if (NowTime == 00)
             {
-                if(TimeNotification == 0)
+                if (TimeNotification == 0)
                 {
                     TimeNotification++;
                     new XSNotifier().SendNotification(new XSNotification() { Title = DateTime.Now.ToString("HHmm"), Content = "現在の時間は" + DateTime.Now.ToString("HH時mm分") + "です" });
@@ -96,22 +96,15 @@ while (true)
             if (JoinNotification == 1)
             {
                 new XSNotifier().SendNotification(new XSNotification() { Title = "Join [" + NumberOfPlayer.ToString() + "]", Content = JoinData });
-                if (NumberOfPlayer > 20)
-                {
-                    new XSNotifier().SendNotification(new XSNotification() { Title = "There are over 20 people in this instance.", Content = NumberOfPlayer.ToString() });
-                }
+
             }
-            if(LeftNotification == 1)
+            if (LeftNotification == 1)
             {
-                new XSNotifier().SendNotification(new XSNotification() { Title = "Left ["+NumberOfPlayer.ToString()+"]", Content = LeftData });
-                if (NumberOfPlayer > 20)
-                {
-                    new XSNotifier().SendNotification(new XSNotification() { Title = "There are over 20 people in this instance.", Content = NumberOfPlayer.ToString() });
-                }
+                new XSNotifier().SendNotification(new XSNotification() { Title = "Left [" + NumberOfPlayer.ToString() + "]", Content = LeftData });
             }
             if (UrlNotification == 1)
             {
-                new XSNotifier().SendNotification(new XSNotification() { Title = "URL", Content = UrlData, }) ;
+                new XSNotifier().SendNotification(new XSNotification() { Title = "URL", Content = UrlData, });
             }
 
         }
